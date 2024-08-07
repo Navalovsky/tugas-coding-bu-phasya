661f5b4ac0be3a03703f2a8787dfe71b3daf55f9
Navalovsky <navalovsky_allenzo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:41:27 +0700

    Add files via upload

---
+++ TestGuru.java
@@ -0,0 +1,66 @@
+//Driver Class
+public class TestGuru {
+    public static void main(String[] args) {
+        //membuat obyek
+        Guru buPhasya = new Guru();
+        Guru buAul= new Guru();
+        Guru pakFirman = new Guru();
+        Guru buFeni = new Guru();
+        Guru pakDiaur = new Guru();
+
+        buPhasya.id = 1234;
+        buPhasya.nama = "buPhasya";
+        buPhasya.mapel = "produktif RPL";
+        buPhasya.alamat = "malang";
+        System.out.println("ini data buPhasya");
+        System.out.println(buPhasya.id);
+        System.out.println(buPhasya.nama);
+        System.out.println(buPhasya.mapel);
+        System.out.println(buPhasya.alamat);
+        
+        buAul.id = 11111;
+        buAul.nama = "buAul";
+        buAul.mapel = "produktif RPL";
+        buAul.alamat = "New York";
+        System.out.println("ini data buAul");
+        System.out.println(buAul.id);
+        System.out.println(buAul.nama);
+        System.out.println(buAul.mapel);
+        System.out.println(buAul.alamat);
+        
+        pakFirman.id = 23321;
+        pakFirman.nama = "pakFirman";
+        pakFirman.mapel = "tatib";
+        pakFirman.alamat = "german";
+        System.out.println("ini data pakFirman");
+        System.out.println(pakFirman.id);
+        System.out.println(pakFirman.nama);
+        System.out.println(pakFirman.mapel);
+        System.out.println(pakFirman.alamat);
+        
+        buFeni.id = 67953;
+        buFeni.nama = "buFeni";
+        buFeni.mapel = "matematika";
+        buFeni.alamat = "mars";
+        System.out.println("ini data buFeni");
+        System.out.println(buFeni.id);
+        System.out.println(buFeni.nama);
+        System.out.println(buFeni.mapel);
+        System.out.println(buFeni.alamat);
+        
+        pakDiaur.id = 354844;
+        pakDiaur.nama = "pakDiaur";
+        pakDiaur.mapel = "fisika dan olahraga";
+        pakDiaur.alamat = "las vegas";
+        System.out.println("ini data pakDiaur");
+        System.out.println(pakDiaur.id);
+        System.out.println(pakDiaur.nama);
+        System.out.println(pakDiaur.mapel);
+        System.out.println(pakDiaur.alamat);       
+       }
+    }
+    
+    
+    
+    
+
